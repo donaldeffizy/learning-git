201019522800e37d8e9ba4d8f26be10dac18956e
donaldeffizy <32887014+donaldeffizy@users.noreply.github.com>
2022-09-07 07:57:39 +0100

    Added first python

---
+++ main.py
@@ -0,0 +1,32 @@
+name = "Donald"
+
+ageYoung = 26
+
+brand = "Meckanzy"
+
+number = [1, 2, 5, 9]
+
+pi = 3.142
+
+isYoung = True
+"""
+print(type(brand))
+print(type(ageYoung))
+print(type(name))
+print(type(pi))
+print(type(number))
+
+"""
+email = f""" 
+hello {name}
+The LORD is 
+my shepard {ageYoung}
+How are you? It was 
+nice talking to you
+"""
+print(type(isYoung))
+print(brand.upper())
+print(brand.__len__())
+print(brand.center(213))
+print("code" not in brand)
+print(email)
